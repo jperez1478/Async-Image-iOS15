1040990a67ca2a5567096395fe53e8cf7f4ab61e
jperez1478 <jessicperez3@gmail.com>
2021-12-28 09:10:28 -0600

    Initial Commit

---
+++ AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by Jessica Perez on 12/28/21.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
